f6287d5cc35e9e7cbb3a70fb38512aa37115c2eb
Nirwan <ny.unpar@gmail.com>
2019-09-05 19:33:28 +0700

    masik di index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import {Photo} from "./entity/Photo";
 
 createConnection().then(async connection => {
+    /* let photo = new Photo();
+    photo.name = "Me and Bears";
+    photo.description = "I am near polar bears";
+    photo.filename = "photo-with-bears.jpg";
+    photo.views = 1;
+    photo.isPublished = true;
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+     return connection.manager
+            .save(photo)
+            .then(photo => {
+                console.log("Photo has been saved. Photo id is", photo.id);
+    }); 
+    
+    await connection.manager.save(photo);
+    console.log("Photo has been saved"); */
+    let savedPhotos = await connection.manager.find(Photo);
+    console.log("All photos from the db: ", savedPhotos);
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
-
+    
 }).catch(error => console.log(error));
